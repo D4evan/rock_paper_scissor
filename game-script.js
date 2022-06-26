2e513a44f6686981b7e58322173e42d833d51042
D4evan <davzamparo@gmail.com>
2022-06-26 15:41:52 +0200

    Added score tracking and five match function.

--- game-script.js
+++ game-script.js
@@ -15,15 +15,32 @@ function playRound(playerChoice, computerChoice){
     playerChoice = playerChoice.toLowerCase();
     if (playerChoice == computerChoice){
     result = "It's a tie, play again";
+        playerScore++
     } else if (playerChoice == "rock" && computerChoice == "scissor"){
-        result = ("You win " + playerChoice + " beats " + computerChoice)
+        result = ("You win " + playerChoice + " beats " + computerChoice);
+        playerScore++
     } else if (playerChoice == "scissor" && computerChoice == "paper"){
         result = ("You win " + playerChoice + " beats " + computerChoice)
+        playerScore++
     } else if (playerChoice == "paper" && computerChoice == "rock"){
-                result = ("You win " + playerChoice + " beats " + computerChoice);
-    } else {result = "You lost because " + computerChoice + " beats " + playerChoice}
-    console.log(result);
+        result = ("You win " + playerChoice + " beats " + computerChoice);
+        playerScore++
+    } else {result = "You lost because " + computerChoice + " beats " + playerChoice
+        computerScore++}
+
+    console.log(result, "\ncomputer score: " + computerScore, "\nplayer score: " + playerScore);
 }
 
 
-console.log(playRound(playerChoice, computerChoice));
+//console.log(playRound(playerChoice, computerChoice));
+
+let playerScore = 0;
+let computerScore = 0;
+
+function game(){
+for (let i = 0; i < 5; i++){
+    console.log(playRound(playerChoice, computerChoice));
+}
+}
+
+console.log(game());
