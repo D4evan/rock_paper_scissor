e3a793f60a8af247ae0b7ea62087b51ee78979bc
D4evan <davzamparo@gmail.com>
2022-06-26 16:41:37 +0200

    cleaned code and added round counter

--- game-script.js
+++ game-script.js
@@ -14,8 +14,44 @@ function playRound(playerChoice, computerChoice){
     computerChoice = computerPlay().toLowerCase();
     playerChoice = prompt("Rock, Paper or scissors?").toLowerCase();
     if (playerChoice == computerChoice){
-    result = "It's a tie, play again";
-    } else if (playerChoice == "rock" && computerChoice == "scissors"){
+    result = "Round number "+roundNumber+"\nIt's a tie, play again";
+    } else if (
+        (playerChoice == "rock" && computerChoice == "scissors") ||
+        (playerChoice == "scissors" && computerChoice == "paper") ||
+        (playerChoice == "paper" && computerChoice == "rock")
+    ){result = "Round number "+roundNumber+"\nYou win because " + playerChoice + " beats " + computerChoice,playerScore++} else if (
+        (computerChoice === 'rock' && playerChoice === 'scissors') ||
+        (computerChoice === 'scissors' && playerChoice === 'paper') ||
+        (computerChoice === 'paper' && playerChoice === 'rock')
+    ){result = "Round number "+roundNumber+"\nYou lost because " + computerChoice + " beats " + playerChoice,
+    computerScore++} else {result = "Choice not valid, you lost your turn"}
+
+    console.log(result, "\ncomputer score: " + computerScore, "\nplayer score: " + playerScore);
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 1;
+
+function game(){
+for (let i = 0; i < 5; i++){
+    playRound(playerChoice, computerChoice);
+    roundNumber++;
+}
+}
+
+function score(playerScore, computerScore){
+    if (playerScore > computerScore){
+        return "GAME OVER. Player wins with "+playerScore+" points, "+"congratulations!"
+    } else {return "GAME OVER. Computer wins with "+computerScore+" points, "+"better luck next time!"}
+}
+
+console.log(game());
+console.log(score(playerScore, computerScore));
+
+// OLD CODE
+
+ /*else if (playerChoice == "rock" && computerChoice == "scissors"){
         result = ("You win because " + playerChoice + " beats " + computerChoice);
         playerScore++
     } else if (playerChoice == "scissors" && computerChoice == "paper"){
@@ -23,32 +59,4 @@ function playRound(playerChoice, computerChoice){
         playerScore++
     } else if (playerChoice == "paper" && computerChoice == "rock"){
         result = ("You win because " + playerChoice + " beats " + computerChoice);
-        playerScore++
-    } else if (
-        (computerChoice === 'rock' && playerChoice === 'scissors') ||
-        (computerChoice === 'scissors' && playerChoice === 'paper') ||
-        (computerChoice === 'paper' && playerChoice === 'rock')
-    ){result = "You lost because " + computerChoice + " beats " + playerChoice
-    computerScore++} else {result = "Choice not valid, you lost your turn"}
-        
-
-    console.log(result, "\ncomputer score: " + computerScore, "\nplayer score: " + playerScore);
-}
-
-let playerScore = 0;
-let computerScore = 0;
-
-function game(){
-for (let i = 0; i < 5; i++){
-    playRound(playerChoice, computerChoice);
-}
-}
-
-function score(playerScore, computerScore){
-    if (playerScore > computerScore){
-        return "Player wins with "+playerScore+" points, "+"congratulations!"
-    } else {return "Computer wins with "+computerScore+" points, "+"better luck next time!"}
-}
-
-console.log(game());
-console.log(score(playerScore, computerScore));
+        playerScore++*/
